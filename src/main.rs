a4732a72f25b85afb4d63c8915d84dedc985dca5
Jose Bovet Derpich <jose.bovet@gmail.com>
2021-11-18 09:52:32 -0300

    fix #2 :use pretty json for multiple hosts

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use tlschecker::Certificate;
 
 fn main() {
     let matches = App::new("TLS Checker")
-        .version("1.0")
+        .version("0.1.3")
         .author("Jose Bovet Derpich. <jose.bovet@gmail.com>")
         .about("TLS/SSL certificate expiration date from command-line checker")
         .arg(
@@ -23,45 +23,50 @@ fn main() {
         .get_matches();
 
     let hosts = matches.values_of("host").unwrap();
-    let json_output = matches.is_present("json");
+
+    let mut certificates = Vec::with_capacity(hosts.len());
 
     for host in hosts {
         match Certificate::from(host) {
             Ok(cert) => {
-                if json_output {
-                    let json = serde_json::to_string(&cert).unwrap();
-                    println!("{}", json);
-                } else {
-                    println!("--------------------------------------");
-                    println!("Issued domain: {}", cert.subject.common_name);
-                    println!("Subject Name :");
-                    println!("\tCountry or Region: {}", cert.subject.country_or_region);
-                    println!("\tState or Province: {}", cert.subject.state_or_province);
-                    println!("\tLocality: {}", cert.subject.locality);
-                    println!("\tOrganizational Unit: {}", cert.subject.organization_unit);
-                    println!("\tOrganization: {}", cert.subject.organization);
-                    println!("\tCommon Name: {}", cert.subject.common_name);
-                    println!("Issuer Name:");
-                    println!("\tCountry or Region: {}", cert.issued.country_or_region);
-                    println!("\tOrganization: {}", cert.issued.organization);
-                    println!("\tCommon Name: {}", cert.issued.common_name);
-                    println!("Valid from: {}", cert.valid_from);
-                    println!("Valid to: {}", cert.valid_to);
-                    println!("Days left: {}", cert.validity_days);
-                    println!("Expired: {}", cert.is_expired);
-                    println!("Certificate version: {}", cert.cert_ver);
-                    println!("Certificate algorithm: {}", cert.cert_alg);
-                    println!("Certificate S/N: {}", cert.cert_sn);
-                    println!("Subject Alternative Names:");
-                    for san in cert.sans {
-                        println!("\tDNS Name: {}", san);
-                    }
-                }
+                certificates.push(cert);
             }
             Err(err) => {
-                println!("Fail to check host: {} ", &err.details);
+                println!("Fail to check host: {}  {} ", &host, &err.details);
             }
         }
     }
+
+    if !matches.is_present("json") {
+        for cert in certificates {
+            println!("--------------------------------------");
+            println!("Issued domain: {}", cert.subject.common_name);
+            println!("Subject Name :");
+            println!("\tCountry or Region: {}", cert.subject.country_or_region);
+            println!("\tState or Province: {}", cert.subject.state_or_province);
+            println!("\tLocality: {}", cert.subject.locality);
+            println!("\tOrganizational Unit: {}", cert.subject.organization_unit);
+            println!("\tOrganization: {}", cert.subject.organization);
+            println!("\tCommon Name: {}", cert.subject.common_name);
+            println!("Issuer Name:");
+            println!("\tCountry or Region: {}", cert.issued.country_or_region);
+            println!("\tOrganization: {}", cert.issued.organization);
+            println!("\tCommon Name: {}", cert.issued.common_name);
+            println!("Valid from: {}", cert.valid_from);
+            println!("Valid to: {}", cert.valid_to);
+            println!("Days left: {}", cert.validity_days);
+            println!("Expired: {}", cert.is_expired);
+            println!("Certificate version: {}", cert.cert_ver);
+            println!("Certificate algorithm: {}", cert.cert_alg);
+            println!("Certificate S/N: {}", cert.cert_sn);
+            println!("Subject Alternative Names:");
+            for san in cert.sans {
+                println!("\tDNS Name: {}", san);
+            }
+        }
+    } else {
+        println!("{}", serde_json::to_string_pretty(&certificates).unwrap());
+    }
+
     exit(0);
 }
